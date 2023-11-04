1791b97b013564deafdd0a39aed0aceb32c01ec4
Githubaments <flynnshane@gmail.com>
2023-11-04 21:52:27 +0000

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -109,9 +109,9 @@ if len(filtered_data) < 100:
             logging.info(f'Geocoding address: {address}')
             lat, lon = get_lat_lon(address)
             if lat is not None and lon is not None:
-                filtered_data.at[index, 'Latitude'] = lat
-                filtered_data.at[index, 'Longitude'] = lon
-                logging.info(f'Updated Latitude: {lat}, Longitude: {lon}')
+                filtered_data.at[index, 'latitude'] = lat
+                filtered_data.at[index, 'longitude'] = lon
+                logging.info(f'Updated latitude: {lat}, longitude: {lon}')
             else:
                 logging.warning(f'Geocoding failed for address: {address}')
 else:
